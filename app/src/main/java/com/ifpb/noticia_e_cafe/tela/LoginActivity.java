a0aa63c679bac89984adab8371312f9410c286f5
MailsonD <mailssondennis@gmail.com>
2019-07-14 15:05:07 -0300

    Reorganizando grid e layouts da página de login

--- app/src/main/java/com/ifpb/noticia_e_cafe/tela/LoginActivity.java
+++ app/src/main/java/com/ifpb/noticia_e_cafe/tela/LoginActivity.java
@@ -2,6 +2,7 @@ package com.ifpb.noticia_e_cafe.tela;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -10,64 +11,120 @@ import android.view.Display;
 import android.view.Gravity;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ifpb.noticia_e_cafe.component.ButtonComponent;
 import com.ifpb.noticia_e_cafe.component.InputField;
+import com.ifpb.noticia_e_cafe.util.DeviceProperties;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayoutForm;
-//    private LinearLayout linearLayoutButtons;
+    private LinearLayout layoutMain;
+    private LinearLayout layoutForm;
+    private LinearLayout layoutCadastro;
     private InputField inputFieldEmail;
     private InputField inputFieldSenha;
     private ButtonComponent btnConfirmar;
-    private EditText textoCadastro;
+    private TextView textoCadastro;
+    private TextView linkCadastro;
+    private int width;
+    private int heigth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        //============ INSTANCIANDO COMPONENTES ===============
-        linearLayoutForm = new LinearLayout(this);
-//        linearLayoutButtons = new LinearLayout(this);
-        inputFieldEmail = new InputField(this,"Email", InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS,100);
-        inputFieldSenha = new InputField(this,"Senha", InputType.TYPE_TEXT_VARIATION_PASSWORD,100);
-        btnConfirmar = new ButtonComponent(this,"Entrar", 50, Color.GREEN);
-        textoCadastro= new EditText(this);
-        textoCadastro.setText("Cadastre-se");
-        textoCadastro.setGravity(Gravity.BOTTOM);
-        textoCadastro.setClickable(true);
-
-        //============== CONFIGURANDO LAYOUT ==================
-        linearLayoutForm.setLayoutParams(new LinearLayout.LayoutParams(
+        //========== CONFIGURANDO LAYOUT PRINCIPAL =============
+        layoutMain = new LinearLayout(this);
+        layoutMain.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         );
-        linearLayoutForm.setGravity(Gravity.CENTER);
-        linearLayoutForm.setOrientation(LinearLayout.VERTICAL);
+        layoutMain.setGravity(Gravity.CENTER_HORIZONTAL);
+        layoutMain.setPadding(0,0,0,0);
+        layoutMain.setOrientation(LinearLayout.VERTICAL);
 
-        //=====================================================
-//        linearLayoutButtons.setLayoutParams(new LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-//        );
-//        linearLayoutButtons.setGravity(Gravity.CENTER);
-//        linearLayoutButtons.setOrientation(LinearLayout.VERTICAL);
+        width = DeviceProperties.getDeviceWidth(getWindowManager());
+        heigth = DeviceProperties.getDeviceHeight(getWindowManager());
 
-        //========= ADICIONANDO COMPONENTES AO LAYOUT =========
-        setContentView(linearLayoutForm);
-        linearLayoutForm.addView(inputFieldEmail);
-        linearLayoutForm.addView(inputFieldSenha);
-        linearLayoutForm.addView(btnConfirmar);
-        linearLayoutForm.addView(textoCadastro);
+        configurandoImagem();
+        configurandoFormulario();
+        configurandoTextoDeCadastro();
 
+
+        //===== ADICIONANDO COMPONENTES AO LAYOUT PRINCIPAL =====
+        setContentView(layoutMain);
+        layoutMain.addView(layoutForm);
+        layoutMain.addView(layoutCadastro);
+
+    }
+
+
+
+
+
+    private void configurandoImagem() {
+
+    }
+
+    private void configurandoFormulario() {
+        //====== CONFIGURANDO LAYOUT DO FORMULARIO ========
+        layoutForm = new LinearLayout(this);
+        layoutForm.setOrientation(LinearLayout.VERTICAL);
+        layoutForm.setGravity(Gravity.CENTER_HORIZONTAL);
+        layoutForm.setPadding(10,heigth/8,10,10);
+
+        inputFieldEmail = new InputField(
+                this,"Email", InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS,width
+        );
+        inputFieldSenha = new InputField(
+                this,"Senha", InputType.TYPE_TEXT_VARIATION_PASSWORD,width
+        );
+        btnConfirmar = new ButtonComponent(
+                this,"Entrar", width/3, Color.GREEN
+        );
 
         //=========== ADICIONANDO EVENTOS ONCLICK ==============
 
-        btnConfirmar.setOnClickAction((v) -> Log.d("DEBUGANDO_SAPORRA","MeuBotão"));
         btnConfirmar.setOnClickAction(
-                (v) -> onNewIntent(new Intent(this,TelaCadastro.class))
+                (v) -> Log.d("DEBUGANDO_SAPORRA","MeuBotão")
         );
 
+        //========= ADICIONANDO COMPONENTES AO LAYOUT =========
+
+        layoutForm.addView(inputFieldEmail);
+        layoutForm.addView(inputFieldSenha);
+        layoutForm.addView(btnConfirmar);
 
     }
+
+    private void configurandoTextoDeCadastro() {
+        //======= CONFIGURANDO LAYOUT DO LINK DE CADASTRO ======
+        layoutCadastro = new LinearLayout(this);
+        layoutCadastro.setGravity(Gravity.CENTER_HORIZONTAL);
+//        layoutCadastro.setVerticalGravity(Gravity.BOTTOM);
+        layoutCadastro.setOrientation(LinearLayout.HORIZONTAL);
+        layoutCadastro.setPadding(10,10,10,10);
+
+        textoCadastro= new TextView(this);
+        linkCadastro = new TextView(this);
+
+        textoCadastro.setText("Não possui uma conta ainda?");
+        linkCadastro.setText("Cadastre-se aqui!");
+        linkCadastro.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        linkCadastro.setClickable(true);
+
+
+        //=========== ADICIONANDO EVENTOS ONCLICK ==============
+        linkCadastro.setOnClickListener(
+                (v) -> startActivity(new Intent(this,TelaCadastro.class))
+        );
+
+        //========= ADICIONANDO COMPONENTES AO LAYOUT =========
+        layoutCadastro.addView(textoCadastro);
+        layoutCadastro.addView(linkCadastro);
+    }
+
+
+
 }
