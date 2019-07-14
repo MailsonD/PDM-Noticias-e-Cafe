12ac8d1abae742c95ad3de4d135116aae9b95373
MailsonD <mailssondennis@gmail.com>
2019-07-14 16:37:04 -0300

    Adicionando imagem a tela de login e redimencionando

--- app/src/main/java/com/ifpb/noticia_e_cafe/tela/LoginActivity.java
+++ app/src/main/java/com/ifpb/noticia_e_cafe/tela/LoginActivity.java
@@ -1,18 +1,20 @@
 package com.ifpb.noticia_e_cafe.tela;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
-import android.view.Display;
 import android.view.Gravity;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.ifpb.noticia_e_cafe.R;
 import com.ifpb.noticia_e_cafe.component.ButtonComponent;
 import com.ifpb.noticia_e_cafe.component.InputField;
 import com.ifpb.noticia_e_cafe.util.DeviceProperties;
@@ -20,6 +22,7 @@ import com.ifpb.noticia_e_cafe.util.DeviceProperties;
 public class LoginActivity extends AppCompatActivity {
 
     private LinearLayout layoutMain;
+    private LinearLayout layoutImage;
     private LinearLayout layoutForm;
     private LinearLayout layoutCadastro;
     private InputField inputFieldEmail;
@@ -27,8 +30,11 @@ public class LoginActivity extends AppCompatActivity {
     private ButtonComponent btnConfirmar;
     private TextView textoCadastro;
     private TextView linkCadastro;
+    private ImageView imageView;
     private int width;
     private int heigth;
+
+//    private Resources res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +60,7 @@ public class LoginActivity extends AppCompatActivity {
 
         //===== ADICIONANDO COMPONENTES AO LAYOUT PRINCIPAL =====
         setContentView(layoutMain);
+        layoutMain.addView(layoutImage);
         layoutMain.addView(layoutForm);
         layoutMain.addView(layoutCadastro);
 
@@ -64,7 +71,19 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private void configurandoImagem() {
+        //======== CONFIGURANDO LAYOUT DA IMAGEM =========
+        layoutImage = new LinearLayout(this);
+        layoutImage.setOrientation(LinearLayout.VERTICAL);
+        layoutImage.setGravity(Gravity.CENTER_HORIZONTAL);
+        layoutImage.setPadding(0,25,0,0);
+        layoutImage.setLayoutParams(new LinearLayout.LayoutParams(
+                width/2, (int) ((heigth/2)*0.8))
+        );
 
+        imageView = new ImageView(this);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.logo));
+
+        layoutImage.addView(imageView);
     }
 
     private void configurandoFormulario() {
@@ -72,7 +91,7 @@ public class LoginActivity extends AppCompatActivity {
         layoutForm = new LinearLayout(this);
         layoutForm.setOrientation(LinearLayout.VERTICAL);
         layoutForm.setGravity(Gravity.CENTER_HORIZONTAL);
-        layoutForm.setPadding(10,heigth/8,10,10);
+        layoutForm.setPadding(10,0,10,10);
 
         inputFieldEmail = new InputField(
                 this,"Email", InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS,width
@@ -81,7 +100,7 @@ public class LoginActivity extends AppCompatActivity {
                 this,"Senha", InputType.TYPE_TEXT_VARIATION_PASSWORD,width
         );
         btnConfirmar = new ButtonComponent(
-                this,"Entrar", width/3, Color.GREEN
+                this,"Entrar", width/3, Color.rgb(0,128,0)
         );
 
         //=========== ADICIONANDO EVENTOS ONCLICK ==============
