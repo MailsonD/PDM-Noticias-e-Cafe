be288b897af43416dcb7211e6cfa8df9050e4664
Leanderson Coelho <leanderson.coelhoif@gmail.com>
2019-07-14 13:53:59 -0300

    Atualização da tela de editar usuario para usar a classe DeviceProperties

--- app/src/main/java/com/ifpb/noticia_e_cafe/tela/TelaEditar.java
+++ app/src/main/java/com/ifpb/noticia_e_cafe/tela/TelaEditar.java
@@ -1,79 +1,91 @@
 package com.ifpb.noticia_e_cafe.tela;
 
 import android.graphics.Color;
-import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.Display;
 import android.view.Gravity;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.ifpb.noticia_e_cafe.component.ButtonComponent;
 import com.ifpb.noticia_e_cafe.component.InputField;
+import com.ifpb.noticia_e_cafe.util.DeviceProperties;
 
 public class TelaEditar extends AppCompatActivity {
+
+    private LinearLayout layoutInputs;
+    private LinearLayout layoutButtons;
+    private LinearLayout layoutMain;
+    private TextView nomeTela;
+    private InputField nome;
+    private InputField email;
+    private InputField senha;
+    private ButtonComponent salvar;
+    private ButtonComponent cancelar;
+    private int width;
+
+    public TelaEditar(){}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.width = DeviceProperties.getDeviceWidth(getWindowManager());
 
-        final LinearLayout layoutInputs = new LinearLayout(this);
-        layoutInputs.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        layoutInputs.setOrientation(LinearLayout.VERTICAL);
-        layoutInputs.setPadding(10,40,10,10);
-        layoutInputs.setGravity(Gravity.CENTER_HORIZONTAL);
-
-        final LinearLayout layoutButtons = new LinearLayout(this);
-        layoutButtons.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        layoutButtons.setOrientation(LinearLayout.HORIZONTAL);
-        layoutButtons.setPadding(9,9,9,9);
-        layoutButtons.setGravity(Gravity.CENTER_HORIZONTAL);
-
-        final LinearLayout layoutMain = new LinearLayout(this);
-        layoutMain.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        layoutMain.setOrientation(LinearLayout.VERTICAL);
-        layoutMain.setPadding(0,0,0,0);
-        layoutMain.setGravity(Gravity.CENTER_VERTICAL);
+        //Layout principal
+        this.layoutMain = new LinearLayout(this);
+        this.layoutMain.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        this.layoutMain.setOrientation(LinearLayout.VERTICAL);
+        this.layoutMain.setPadding(0,0,0,0);
+        this.layoutMain.setGravity(Gravity.CENTER_VERTICAL);
         setContentView(layoutMain);
 
         //Titulo da tela
-        final TextView nomeTela = new TextView(this);
-        nomeTela.setText("Editar informações");
-        nomeTela.setTextSize(30f);
-        nomeTela.setPadding(100,50,0,0);
-        nomeTela.setTextColor(Color.rgb(7,8,8));
+        this.nomeTela = new TextView(this);
+        this.nomeTela.setText("Editar informações");
+        this.nomeTela.setTextSize(30f);
+        this.nomeTela.setPadding(100,50,0,0);
+        this.nomeTela.setTextColor(Color.rgb(7,8,8));
 
-        //Pegar informações da tela do dispositivo
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
+        this.construirLayoutInputs();
+        this.construirLayoutBotoes();
+
+        this.layoutMain.addView(nomeTela);
+        this.layoutMain.addView(layoutInputs);
+        this.layoutMain.addView(layoutButtons);
+
+    }
+
+    private void construirLayoutInputs(){
+        this.layoutInputs = new LinearLayout(this);
+        this.layoutInputs.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        this.layoutInputs.setOrientation(LinearLayout.VERTICAL);
+        this.layoutInputs.setPadding(10,40,10,10);
+        this.layoutInputs.setGravity(Gravity.CENTER_HORIZONTAL);
 
         //Inputs para edição das informações
-        final InputField nome = new InputField(this,"Nome", InputType.TYPE_CLASS_TEXT,width);
-        final InputField email = new InputField(this, "Email", InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS,width);
-        final InputField senha = new InputField(this, "Senha", InputType.TYPE_TEXT_VARIATION_PASSWORD,width);
+        this.nome = new InputField(this,"Nome", InputType.TYPE_CLASS_TEXT, this.width);
+        this.email = new InputField(this, "Email", InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS, this.width);
+        this.senha = new InputField(this, "Senha", InputType.TYPE_TEXT_VARIATION_PASSWORD, this.width);
+
+        this.layoutInputs.addView(nome);
+        this.layoutInputs.addView(email);
+        this.layoutInputs.addView(senha);
+    }
+
+    private void construirLayoutBotoes(){
+        this.layoutButtons = new LinearLayout(this);
+        this.layoutButtons.setLayoutParams(new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        this.layoutButtons.setOrientation(LinearLayout.HORIZONTAL);
+        this.layoutButtons.setPadding(9,9,9,9);
+        this.layoutButtons.setGravity(Gravity.CENTER_HORIZONTAL);
 
         //Botões
-        final ButtonComponent salvar = new ButtonComponent(this,"Salvar",width/2,Color.rgb(0,128,0));
-        final ButtonComponent cancelar = new ButtonComponent(this,"Cancelar",width/2, Color.rgb(255,0,0));
+        this.salvar = new ButtonComponent(this,"Salvar",this.width/2,Color.rgb(0,128,0));
+        this.cancelar = new ButtonComponent(this,"Cancelar",this.width/2, Color.rgb(255,0,0));
 
-        layoutInputs.addView(nome);
-        layoutInputs.addView(email);
-        layoutInputs.addView(senha);
-
-        layoutButtons.addView(cancelar);
-        layoutButtons.addView(salvar);
-
-        layoutMain.addView(nomeTela);
-        layoutMain.addView(layoutInputs);
-        layoutMain.addView(layoutButtons);
-
+        this.layoutButtons.addView(cancelar);
+        this.layoutButtons.addView(salvar);
 
     }
 }
